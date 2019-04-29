390248446cea18e3b4123dbed808ab2e1f5d65c8
simennj <simennj@outlook.com>
2019-04-29 11:18:17 +0200

    add endpoint for sorted fmu variables

--- src/fmus/views.py
+++ src/fmus/views.py
@@ -3,6 +3,7 @@ import os
 from aiohttp import web
 from fmpy import read_model_description
 
+from src import simulation
 from src.utils import dumps, find_in_dir, RouteTableDefDocs
 
 routes = RouteTableDefDocs()
@@ -20,7 +21,19 @@ async def fmu_list(request: web.Request):
 
 @routes.get('/fmus/{id}', name='fmu_detail')
 async def fmu_detail(request: web.Request):
-    """Get detailed information for the FMU with the given id"""
+    """Get detailed information for the FMU with the given id
+
+    Append /vars to get sorted variables
+    """
     file = await find_in_dir(request.match_info['id'], request.app['settings'].FMU_DIR)
     model_description = read_model_description(file)
     return web.json_response(model_description, dumps=dumps)
+
+
+@routes.get('/fmus/{id}/vars', name='fmu_vars')
+async def fmu_vars(request: web.Request):
+    """Get sorted variables for the FMU with the given id"""
+    file = await find_in_dir(request.match_info['id'], request.app['settings'].FMU_DIR)
+    model_variables = simulation.ModelVariables(read_model_description(file))
+    return web.json_response(model_variables, dumps=dumps)
+
