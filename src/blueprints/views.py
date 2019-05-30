745dde8f389f86e62dea459c3a59e0dd6fa4326f
simennj <simennj@outlook.com>
2019-05-30 14:12:45 +0200

    prepare blueprints views code for delivery

--- src/blueprints/views.py
+++ src/blueprints/views.py
@@ -1,5 +1,6 @@
 import ast
 import os
+from typing import Tuple, List
 
 from aiohttp import web
 
@@ -26,25 +27,29 @@ async def blueprint_detail(request: web.Request):
         tree = ast.parse(f.read(), filename='__init__.py')
     docs = ast.get_docstring(tree)
     process_class = list(n for n in tree.body if isinstance(n, ast.ClassDef) and n.name == 'P')[0]
-    init_docs, init_params = await retrieve_function_info(process_class.body, '__init__')
-    start_docs, start_params = await retrieve_function_info(process_class.body,'start', 2)
-    # input_names_def = list(n for n in process_class.body if isinstance(n, ast.Assign) and 'input_names' == n.targets[0].id)[-1].value
-    # input_names = [name.s for name in input_names_def.elts]
+    init_docs, init_params = await retrieve_method_info(process_class.body, '__init__')
+    start_docs, start_params = await retrieve_method_info(process_class.body, 'start', 2)
     return web.json_response({
         'docs': docs,
         'init_docs': init_docs,
         'init_params': init_params,
         'start_docs': start_docs,
         'start_params': start_params,
-        # 'input_names': input_names,
     }, dumps=dumps)
 
 
-async def retrieve_function_info(body, function_name, end_ignore_args_index=1):
-    function = list(n for n in body if isinstance(n, ast.FunctionDef) and n.name == function_name)[0]
-    docs = ast.get_docstring(function)
+async def retrieve_method_info(class_body, method_name, params_ignore=1) -> Tuple[str, List]:
+    """Retrieves docs and parameters from the method
+
+    :param class_body: the body of the class the method belongs to
+    :param method_name: the name of the method
+    :param params_ignore: how many of the first params to ignore, defaults to 1 (only ignore self)
+    :return: a tuple containing both the docstring of the method and a list of parameters with name and default value
+    """
+    function = list(n for n in class_body if isinstance(n, ast.FunctionDef) and n.name == method_name)[0]
+    docs: str = ast.get_docstring(function)
     params = [{'name': a.arg, 'default': ''}
-                    for a in function.args.args[end_ignore_args_index:]]
+              for a in function.args.args[params_ignore:]]
     defaults_start_index = len(params) - len(function.args.defaults)
     for i, v in enumerate(function.args.defaults):
         params[defaults_start_index + i]['default'] = getattr(v, v._fields[0], '')
