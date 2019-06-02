4748444493d59f7a5a6a3742e627773ffe348b1d
simennj <simennj@outlook.com>
2019-06-02 15:49:32 +0200

    fix fft blueprint not following new interface rules

--- files/blueprints/fft/__init__.py
+++ files/blueprints/fft/__init__.py
@@ -19,7 +19,7 @@ class P:
         self.sample_spacing = sample_spacing
         self.window = window
         self.measurements = np.zeros(self.window, dtype=float)
-        self.inputs = [0]
+        self.input_values = [0]
         self.frequencies = np.fft.rfftfreq(self.window, self.sample_spacing)
         self.output_names = tuple(str(f) for f in self.frequencies)
         self.output_values = [np.zeros(self.window, dtype=float)]
@@ -30,7 +30,7 @@ class P:
 
     def set_inputs(self, input_refs: List[int], input_values: List[int]):
         for i in range(len(input_refs)):
-            self.inputs[input_refs[i]] = input_values[i]
+            self.input_values[input_refs[i]] = input_values[i]
 
     def get_outputs(self, output_refs: List[int]):
         self.output_values = np.abs(np.fft.rfft(self.measurements)) ** 2
@@ -43,5 +43,5 @@ class P:
         # print(f'after current_time: {t}, self.time: {self.t}')
         while t >= self.t or math.isclose(t, self.t, rel_tol=1e-15):
             self.t += self.sample_spacing
-            self.measurements[0] = self.inputs[0]
+            self.measurements[0] = self.input_values[0]
             self.measurements = np.roll(self.measurements, -1)
