ec745114c427e8665cb30d7678be83a7dd781010
simennj <simennj@outlook.com>
2019-06-02 15:40:47 +0200

    remove use of simulator in fmus views

--- src/fmus/views.py
+++ src/fmus/views.py
@@ -3,7 +3,6 @@ import os
 from aiohttp import web
 from fmpy import read_model_description
 
-from src import simulation
 from src.utils import dumps, find_in_dir, RouteTableDefDocs
 
 routes = RouteTableDefDocs()
@@ -23,17 +22,16 @@ async def fmu_list(request: web.Request):
 async def fmu_detail(request: web.Request):
     """Get detailed information for the FMU with the given id
 
-    Append /vars to get sorted variables
     """
     file = await find_in_dir(request.match_info['id'], request.app['settings'].FMU_DIR)
     model_description = read_model_description(file)
     return web.json_response(model_description, dumps=dumps)
 
 
-@routes.get('/fmus/{id}/vars', name='fmu_vars')
-async def fmu_vars(request: web.Request):
-    """Get sorted variables for the FMU with the given id"""
-    file = await find_in_dir(request.match_info['id'], request.app['settings'].FMU_DIR)
-    model_variables = simulation.ModelVariables(read_model_description(file))
-    return web.json_response(model_variables, dumps=dumps)
+# @routes.get('/fmus/{id}/vars', name='fmu_vars')
+# async def fmu_vars(request: web.Request):
+#     """Get sorted variables for the FMU with the given id"""
+#     file = await find_in_dir(request.match_info['id'], request.app['settings'].FMU_DIR)
+#     model_variables = simulation.ModelVariables(read_model_description(file))
+#     return web.json_response(model_variables, dumps=dumps)
 
